96eb3c881ff07e2dab27c07ba1f36da36321a15d
mahamudul islam <mrrahul2656@gmail.com>
2024-10-01 11:16:09 +0600

    student update

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,5 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
-    protected $fillable = ['first_name','last_name','full_name','dob','roll','email','mobile','photo','age','class','year','isActived','isDeleted'];
+    protected $fillable = ['first_name','last_name','full_name','dob','roll','email','mobile','photo','age','fees_type_id','class_id','section_id','academic_year_id','isActived','isDeleted'];
+
+public function AddFess(){
+
+    return $this->belongsTo(AddFessType::class);
 }
+public function classes(){
+
+    return $this->belongsTo(AddClass::class);
+}
+public function Section(){
+
+    return $this->belongsTo(AddSection::class);
+}
+public function Academy(){
+
+    return $this->belongsTo(AddAcademy::class);
+}
+
+}
+
