8331a0d32a346a779f51212f18a75efbb6f954f7
Mohibbur Rahman <mohibbur1994@gmail.com>
2024-10-02 20:05:11 +0600

    donors error solve

--- app/Http/Controllers/DonorController.php
+++ app/Http/Controllers/DonorController.php
@@ -23,9 +23,9 @@ class DonorController extends Controller
     // Show the form for creating a new student
     public function create()
     {
-        
-         $fees_types = AddFessType::all(); // Fetch all available fees types
-        
+
+        $fees_types = AddFessType::all(); // Fetch all available fees types
+
         return view('donors.create', compact('fees_types'));
     }
     public function store(Request $request)
@@ -39,7 +39,7 @@ class DonorController extends Controller
             'isActived' => 'boolean',
         ]);
 
-       
+
         // Create a new student record using the validated data
         Donor::create([
             'name' => $validatedData['name'],
@@ -61,30 +61,30 @@ class DonorController extends Controller
     }
     public function edit($id)
     {
-       // Find the class by ID
-       $Donor = Donor::findOrFail($id);
-       $Donors = Donor::all();
+        // Find the class by ID
+        $Donor = Donor::findOrFail($id);
+        $fees_types = AddFessType::all();
 
         // Return view with the class details for editing
-        return view('donors.create', compact('Donor', 'Donors'));
+        return view('donors.create', compact('Donor', 'fees_types'));
     }
     public function update(Request $request, Donor $Donor)
     {
         // Validate the incoming request data
         $validatedData = $request->validate([
-           'name' => 'nullable|string|max:255',
+            'name' => 'nullable|string|max:255',
             'email' => 'nullable|email|max:255',
             'mobile' => 'nullable|string|max:15',
-            'fees_type_id' => 'required|string|max:255',
+            'fees_type_id' => 'required|exists:add_fess_types,id',
             'isActived' => 'boolean',
         ]);
 
-       
+
         // Update the student record using the validated data
         $Donor->update([
-            'name' => $validatedData['name'], 
-            'email' => $validatedData['email'],
+            'name' => $validatedData['name'],
             'mobile' => $validatedData['mobile'],
+            'email' => $validatedData['email'],
             'fees_type_id' => $validatedData['fees_type_id'],
             'isActived' => $validatedData['isActived'] ?? 1, // Set active by default if not provided
         ]);
